66bf9471cb2bfe5e61bd97e5401247dc0e411ef9
Ayushgoel554 <94683937+Ayushgoel554@users.noreply.github.com>
2021-11-19 16:20:25 +0530

    Create Write a Java Program to reverse the letters present in the given String.java

---
+++ Write a Java Program to reverse the letters present in the given String.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String :");
+		String a=sc.next();
+		String b="";
+		for(int i= a.length()-1;i>=0;i--)
+		{
+		    b= b+a.charAt(i);
+		}
+	    System.out.println("reverse of String : "+b);
+	}
+}
+
+
+
