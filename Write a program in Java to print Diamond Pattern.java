ebe3abce5754335ba9cbb42c8c4f68ea81d9904f
Ayushgoel554 <94683937+Ayushgoel554@users.noreply.github.com>
2021-11-19 16:19:24 +0530

    Create Write a program in Java to print Diamond Pattern.java

---
+++ Write a program in Java to print Diamond Pattern.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter input : ");
+	    int n=sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {   
+	        for(int j=n;j>=i;j--)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int j=1;j<=i;j++)
+	        {
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	       for(int i=5;i>=1;i--)
+	    {   
+	        for(int j=n;j>=i;j--)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int j=1;j<=i;j++)
+	        {
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
+
+
